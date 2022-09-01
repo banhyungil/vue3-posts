6bea8d297b6d9485438d2bc2b10c6c39fc8adc6c
banhyungil <byban4186@gmail.com>
2022-09-01 16:15:49 +0900

    Modal창 생성, Nested Component 구조에서 v-model 적용

--- src/views/posts/PostListView.vue
+++ src/views/posts/PostListView.vue
@@ -8,12 +8,13 @@
     ></PostFilter>
     <hr class="my-4" />
     <AppGrid :items="posts">
-      <template v-slot="{ item }">
+      <template v-slot:default="{ item }">
         <PostItem
           :title="item.title"
           :contents="item.contents"
           :created-at="item.createdAt"
           @click="goPage(item.id)"
+          @modal="openModal(item)"
         ></PostItem>
       </template>
     </AppGrid>
@@ -21,6 +22,18 @@
       v-model:curPage="reqParams._page"
       :page-count="pageCount"
     ></AppPagination>
+
+    <Teleport to="#modal">
+      <PostModal
+        v-model="show"
+        :title="modalTitle"
+        :contents="modalContents"
+        :createdAt="modalCreatedAt"
+      ></PostModal>
+    </Teleport>
+    <Teleport to="#modal">
+      <div>Hello World!</div>
+    </Teleport>
     <hr class="my-5" />
     <AppCard>
       <PostDetail :id="'1'"></PostDetail>
@@ -35,6 +48,7 @@ import AppCard from '@/components/AppCard.vue';
 import AppPagination from '@/components/AppPagination.vue';
 import AppGrid from '@/components/AppGrid.vue';
 import PostFilter from '@/components/posts/PostFilter.vue';
+import PostModal from '@/components/posts/PostModal.vue';
 import { getPosts } from '@/api/posts.js';
 import { ref, watchEffect } from 'vue';
 import { useRouter } from 'vue-router';
@@ -45,7 +59,7 @@ const posts = ref([]);
 const reqParams = ref({
   _sort: 'createdAt',
   _order: 'desc',
-  _limit: '3',
+  _limit: 3,
   _page: 1,
   title_like: '',
 });
@@ -82,6 +96,18 @@ const goPage = (id) => {
       id,
     },
   });
+};
+
+// modal
+const show = ref(false);
+const modalTitle = ref('');
+const modalContents = ref('');
+const modalCreatedAt = ref('');
+const openModal = ({ title, contents, createdAt }) => {
+  show.value = true;
+  modalTitle.value = title;
+  modalContents.value = contents;
+  modalCreatedAt.value = createdAt;
 };
 </script>
 
