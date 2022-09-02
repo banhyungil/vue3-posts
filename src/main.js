2cd4c910a18b9d98bfec56a5342e08c50a1b7b45
banhyungil <byban4186@gmail.com>
2022-09-02 14:03:35 +0900

    plugin 작성 및 등록 예제 생성, HomeView에 적용

--- src/main.js
+++ src/main.js
@@ -3,8 +3,16 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from '@/router';
+import funcPlugins from './plugins/func';
+import objPlugins from './plugins/obj';
+import person from './plugins/person';
 
-createApp(App).use(router).mount('#app');
+const app = createApp(App);
+app.use(funcPlugins);
+app.use(objPlugins, { name: 'HalfRoad' });
+app.use(person, { name: '홍길동1' });
+app.use(router);
+app.mount('#app');
 import 'bootstrap/dist/js/bootstrap.js';
 
 // console.log('MODE: ', import.meta.env.MODE);
